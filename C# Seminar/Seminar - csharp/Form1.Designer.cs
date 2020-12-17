1e4bd5b96256351bff2f55e9a7e7479945a297f1
IBukvic <73281799+IBukvic@users.noreply.github.com>
2020-12-17 19:59:59 +0100

    Add files via upload

---
+++ C# Seminar/Seminar - csharp/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Seminar___csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.file = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveOutputToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(321, 354);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(192, 383);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(141, 41);
+            this.Calculate.TabIndex = 1;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(362, 23);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(359, 372);
+            this.textBox2.TabIndex = 2;
+            // 
+            // file
+            // 
+            this.file.Location = new System.Drawing.Point(12, 383);
+            this.file.Name = "file";
+            this.file.Size = new System.Drawing.Size(125, 41);
+            this.file.TabIndex = 3;
+            this.file.Text = "Open File";
+            this.file.UseVisualStyleBackColor = false;
+            this.file.Click += new System.EventHandler(this.file_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(808, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem,
+            this.saveOutputToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.closeToolStripMenuItem.Text = "&Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // saveOutputToolStripMenuItem
+            // 
+            this.saveOutputToolStripMenuItem.Name = "saveOutputToolStripMenuItem";
+            this.saveOutputToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.saveOutputToolStripMenuItem.Text = "&Save Output";
+            this.saveOutputToolStripMenuItem.Click += new System.EventHandler(this.saveOutputToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 436);
+            this.Controls.Add(this.file);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Statistika riječi";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button file;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveOutputToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ C# Seminar/Seminar - csharp/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Seminar___csharp
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Calculate_Click(object sender, EventArgs e)
+        { //Button za izracun
+            textBox2.Clear();
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            string line;
+            for (int i = 0; i < textBox1.Lines.Length; i++)
+            {
+                line = textBox1.Lines[i];
+                line = Regex.Replace(line, @"[^a-zA-Z ]", String.Empty);
+                string[] word = line.Split();
+                foreach (string s in word)
+                {
+                    if (s.Length < 2) continue;
+                    if (words.ContainsKey(s) == true)
+                    {
+                        words[s]++;
+                    }
+                    else
+                    {
+                        words.Add(s, 1);
+                    }
+                }
+
+            }
+            var newList = words.OrderByDescending(x => x.Value).ToList();
+            textBox2.Text = "Riječ : Broj Pojavljivanja" + Environment.NewLine;
+            textBox2.Text += "_______________________" + Environment.NewLine;
+            textBox2.Text += Environment.NewLine;
+            foreach (KeyValuePair<string, int> pair in newList) 
+            {
+                string par = $"{pair.Key} : {pair.Value}";
+                textBox2.Text += par + Environment.NewLine;
+            }
+        }
+
+        private void file_Click(object sender, EventArgs e)
+        {//Otvaranje filea pomoću botuna
+            OpenFile();
+        }
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {//Otvaranje filea iz menu-a
+            OpenFile();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {//zatvaranje prozora iz menu-a
+            Close();
+        }
+
+        private void OpenFile()
+        {
+            OpenFileDialog openfile = new OpenFileDialog
+            {
+                Filter = "Text File|*.txt",
+                Title = "Open a Text File"
+            };
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                string path = openfile.FileName;
+                StreamReader reader = new StreamReader(path);
+                string text = reader.ReadToEnd();
+                textBox1.Text = text;
+            }
+        }
+
+        private void saveOutputToolStripMenuItem_Click(object sender, EventArgs e)
+        {//Spremanje outputa u tekstualnu datoteku
+
+            try
+            {
+                SaveFile();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void SaveFile()
+        {
+            SaveFileDialog saveFile = new SaveFileDialog
+            {
+                Filter = "Text File|*.txt",
+                Title = "Save an Text File"
+            };
+            if (textBox2.Text.Length == 0) 
+            {
+                throw new Exception(String.Format("Datoteka se ne može spremiti, textbox je prazan"));
+            }
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                string path = saveFile.FileName;
+                File.WriteAllText(path, textBox2.Text);
+                throw new Exception(String.Format("Datoteka je spremljena"));
+            }
+
+        }           
+
+        private void aboutToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+            about.Show();
+        }        
+    }
+}
